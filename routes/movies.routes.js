f7c7b3def30273fa10f8a8bc3f5f3271bb370496
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-11 23:21:08 +0100

    Iteration 6 started

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,21 @@
 const router = require("express").Router();
+const CelebrityModel = require("./../models/Celebrity.model");
+const MovieModel = require("./../models/Movie.model");
 
 // all your routes here
+
+router.get("/create", async (req, res, next) => {
+  try {
+    res.render("/movies/new-movie", {
+      celebrities: await CelebrityModel.find(),
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+});
 
 module.exports = router;
