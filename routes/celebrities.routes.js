68e8dc7149a5e95737c6237f1a16b049ef8199f3
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-11 18:52:47 +0100

    Iteration 2 upploaded, form uploaded on Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,16 +4,15 @@ const CelebrityModel = require("./../models/Celebrity.model");
 // all your routes here
 
 // GET - create one celebrity (form)
-router.get("/create", async (req, res, next) => {
-  const celebrities = await CelebrityModel.find();
-  res.render("celebrities/new-celebrity", celebrities);
+router.get("/create", async (req, res) => {
+  res.render("celebrities/new-celebrity");
 });
 
 // POST - create one celebrity
 router.post("/create", async (req, res, next) => {
   try {
     await CelebrityModel.create(req.body);
-    res.redirect("");
+    res.redirect("/celebrities");
   } catch (e) {
     next(e);
   }
